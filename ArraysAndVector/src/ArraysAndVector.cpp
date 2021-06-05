7424d26ce6b87cc10792d26b57b56725f667f504
Abdul <samad3911@gmail.com>
2021-06-05 12:07:04 +0530

    Added Vector class

--- ArraysAndVector/src/ArraysAndVector.cpp
+++ ArraysAndVector/src/ArraysAndVector.cpp
@@ -8,10 +8,12 @@
 
 #include <iostream>
 #include <array>
+#include <vector>
 using namespace std;
 
 int main() {
 	array<int, 10> myArray{};
+	vector<string> pets;
 
 	for(int i=0;i<10;i++)
 	{
@@ -23,6 +25,19 @@ int main() {
 		cout<<a<<endl;
 	}
 	cout<<"Size of array is: "<<myArray.size()<<endl;
+
+	pets.push_back("Dog");
+	pets.push_back("Cat");
+	pets.push_back("Goat");
+	pets.push_back("Cow");
+	pets.push_back("Rabbit");
+
+	pets.insert(pets.begin()+2,"John Baug");
+
+	pets.pop_back();
+
+	for(string a: pets)
+		cout<<a<<endl;
 
 	return 0;
 }
